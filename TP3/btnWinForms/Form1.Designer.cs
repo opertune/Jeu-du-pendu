609cbe448f295457a5af6fc927be17ccab2674c5
opertune <59347356+opertune@users.noreply.github.com>
2021-07-16 11:09:46 +0200

    Add files via upload

---
+++ TP3/btnWinForms/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace btnWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpButton = new System.Windows.Forms.GroupBox();
+            this.grpMot = new System.Windows.Forms.GroupBox();
+            this.txtMot = new System.Windows.Forms.TextBox();
+            this.pctPendu = new System.Windows.Forms.PictureBox();
+            this.btnRetry = new System.Windows.Forms.Button();
+            this.lblResultat = new System.Windows.Forms.Label();
+            this.grpMot.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctPendu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpButton
+            // 
+            this.grpButton.Location = new System.Drawing.Point(12, 87);
+            this.grpButton.Name = "grpButton";
+            this.grpButton.Size = new System.Drawing.Size(237, 97);
+            this.grpButton.TabIndex = 0;
+            this.grpButton.TabStop = false;
+            this.grpButton.Text = "lettres";
+            // 
+            // grpMot
+            // 
+            this.grpMot.Controls.Add(this.txtMot);
+            this.grpMot.Location = new System.Drawing.Point(12, 12);
+            this.grpMot.Name = "grpMot";
+            this.grpMot.Size = new System.Drawing.Size(237, 69);
+            this.grpMot.TabIndex = 1;
+            this.grpMot.TabStop = false;
+            this.grpMot.Text = "mot à chercher";
+            // 
+            // txtMot
+            // 
+            this.txtMot.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMot.Location = new System.Drawing.Point(6, 22);
+            this.txtMot.Multiline = true;
+            this.txtMot.Name = "txtMot";
+            this.txtMot.Size = new System.Drawing.Size(225, 40);
+            this.txtMot.TabIndex = 0;
+            this.txtMot.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMot_KeyPress);
+            // 
+            // pctPendu
+            // 
+            this.pctPendu.Image = global::btnWinForms.Properties.Resources.pendu0;
+            this.pctPendu.Location = new System.Drawing.Point(258, 13);
+            this.pctPendu.Name = "pctPendu";
+            this.pctPendu.Size = new System.Drawing.Size(221, 234);
+            this.pctPendu.TabIndex = 3;
+            this.pctPendu.TabStop = false;
+            // 
+            // btnRetry
+            // 
+            this.btnRetry.Image = global::btnWinForms.Properties.Resources.playagain;
+            this.btnRetry.Location = new System.Drawing.Point(201, 199);
+            this.btnRetry.Name = "btnRetry";
+            this.btnRetry.Size = new System.Drawing.Size(48, 48);
+            this.btnRetry.TabIndex = 2;
+            this.btnRetry.UseVisualStyleBackColor = true;
+            this.btnRetry.Click += new System.EventHandler(this.btnRetry_Click);
+            // 
+            // lblResultat
+            // 
+            this.lblResultat.AutoSize = true;
+            this.lblResultat.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultat.Location = new System.Drawing.Point(12, 216);
+            this.lblResultat.Name = "lblResultat";
+            this.lblResultat.Size = new System.Drawing.Size(0, 31);
+            this.lblResultat.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 256);
+            this.Controls.Add(this.lblResultat);
+            this.Controls.Add(this.pctPendu);
+            this.Controls.Add(this.btnRetry);
+            this.Controls.Add(this.grpMot);
+            this.Controls.Add(this.grpButton);
+            this.Name = "Form1";
+            this.Text = "Jeu du Pendu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpMot.ResumeLayout(false);
+            this.grpMot.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctPendu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpButton;
+        private System.Windows.Forms.GroupBox grpMot;
+        private System.Windows.Forms.TextBox txtMot;
+        private System.Windows.Forms.Button btnRetry;
+        private System.Windows.Forms.PictureBox pctPendu;
+        private System.Windows.Forms.Label lblResultat;
+    }
+}
+
---
+++ TP3/btnWinForms/Form1.cs
@@ -0,0 +1,189 @@
+﻿/**
+ * Jeu du Pendu
+ * Date 26/01/2020
+ * Author : Romain
+ */
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace btnWinForms
+{
+    public partial class Form1 : Form
+    {
+        // Déclaration
+        private char[] tAlpha = new char[26];
+        string motachercher, tirets;
+        int erreur = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /**
+         * Module création des boutons dans la groupe box
+         */
+        private void aButton()
+        {
+            int k = 0, xPos = 0, yPos = 15;
+            // Boucle sur la création des 26 boutons
+            while (k < tAlpha.Length)
+            {
+                // Remplissage du tableau avec l'alphabet
+                tAlpha[k] = (char)('A' + k);
+                // Création d'un bouton
+                Button btn = new Button();
+                grpButton.Controls.Add(btn);
+                btn.Size = new Size(25, 25);
+                btn.Location = new Point((xPos*25)+5, yPos);
+                btn.Text = tAlpha[k].ToString(); // Attribue un caractère à chaque bouton
+                grpButton.Controls[k].Enabled = false; // Désactive les boutons
+                k++;
+                xPos++;
+                if (k == 9 || k == 18)
+                {
+                    xPos = 0;
+                    yPos += 25;
+                }
+                btn.Click += new System.EventHandler(btnAlplha_Click); // Evenement clic sur un bouton
+            }
+        }
+        /**
+         * Evenement clic sur un bouton de l'alphabet
+         * Action quand un clic est effectué sur un bouton
+         */
+         private void btnAlplha_Click(object sender, EventArgs e)
+        {
+            // Si le mot ne contient pas la lettre saisie on affiche l'image du pendu
+            if (!motachercher.Contains(((Button)sender).Text.ToLower()))
+            {
+                mauvaiseLettre();
+            }
+            else // Sinon on recherche la position de la lettre dans le mot pour remplacer le tiret
+            {
+                bonneLettre(((Button)sender).Text.ToLower());
+            }
+            ((Button)sender).Enabled = false; // Désactivation de la lettre utilisé
+            resultat(); // Vérification si le joueur a gagné ou perdu
+        }
+        /**
+         * Module bonneLettre
+         * Contient la fonction qui affiche les bonnes lettre dans la texte box
+         */
+        private void bonneLettre(string lettre)
+        {
+            for (int k = 0; k < motachercher.Length; k++)
+            {
+                // Recherche de la position de la lettre dans le mot d'origine
+                if (lettre == motachercher.Substring(k, 1))
+                {
+                    tirets = tirets.Remove(k, 1).Insert(k, lettre); // Supprime le tiret à la position k et le remplace par la bonne / les bonnes lettre(s)
+                    txtMot.Text = tirets; // Affichage la ligne de tirets avec les lettres à la bonne position
+                }
+            }
+        }
+        /**
+         * Module mauvaiseLettre
+         * Contient la fonction qui affiche les images du pendu si la lettre est incorrect
+         */
+        private void mauvaiseLettre()
+        {
+            pctPendu.ImageLocation = Application.StartupPath + "/../../Resources/pendu" + erreur + ".png";
+            erreur++;
+        }
+        /**
+         * Module resultat
+         * Affiche si le joueur à gagné ou perdu
+         */
+         private void resultat()
+        {
+            if(pctPendu.ImageLocation == Application.StartupPath + "/../../Resources/pendu10.png")
+            {
+                lblResultat.Text = "PERDU";
+                txtMot.BackColor = Color.Red;
+                txtMot.Text = motachercher;
+                btnRetry.Focus();
+                for(int k = 0; k < tAlpha.Length; k++)
+                {
+                    grpButton.Controls[k].Enabled = false;
+                }
+            }
+            else
+            {
+                if(txtMot.Text == motachercher)
+                {
+                    lblResultat.Text = "GAGNE";
+                    txtMot.BackColor = Color.LightGreen;
+                    btnRetry.Focus();
+                    for (int k = 0; k < tAlpha.Length; k++)
+                    {
+                        grpButton.Controls[k].Enabled = false;
+                    }
+                }
+            }
+        }
+        /**
+         * Evenement initialisation de la fenetre
+         * Affiche les 26 boutons avec l'alphabet
+         */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            aButton();
+        }
+
+        /**
+         * Evenement click sur le bouton rejouer
+         * Remise à 0 du jeu
+         */
+        private void btnRetry_Click(object sender, EventArgs e)
+        {
+            txtMot.Clear(); // Reset de la zone de saisie du mot
+            txtMot.BackColor = Color.Empty;
+            lblResultat.Text = ""; // Reset de resultat
+            txtMot.Enabled = true; // Activation de la zone de saisie
+            // Reset de l'image
+            pctPendu.ImageLocation = Application.StartupPath + "/../../Resources/pendu0.png";
+            // Reset du nombre d'erreur
+            erreur = 1;
+        }
+
+        /**
+        * Evenement d'une touche pressée dans la zone de saisie
+        * Controle du mot saisie
+        */
+        private void txtMot_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                // Controle de la saisie
+                if ((txtMot.Text.Length < 1) || System.Text.RegularExpressions.Regex.IsMatch(txtMot.Text, "[^a-z]"))
+                {
+                    e.Handled = true;
+                    // Affichage du message d'erreur
+                    MessageBox.Show("Le mot ne doit comporter que des lettres alphabétiques (pas d'espace, pas d'accent, pas de majuscule)");
+                    txtMot.Clear(); // Reset de la txtbox
+                    txtMot.Focus(); // Refocus sur la txtbox
+                }
+                else
+                {
+                    e.Handled = false;
+                    motachercher = txtMot.Text;
+                    // Si la saisie est bonne les lettres sont remplacé par des tirets et les boutons sont déverrouillés
+                    string s = null;
+                    for (int k = 0; k < txtMot.Text.Length; k++)
+                    {
+                        s += "-";
+                    }
+                    txtMot.Text = s; // Affichage des tirets dans la txtbox
+                    tirets = s; // Récupération des tirets dans une variable différente
+                    txtMot.Enabled = false; // Désactivation de la zone de saisie
+                    // Activation des boutons avec les lettres
+                    for (int k = 0; k < tAlpha.Length; k++)
+                    {
+                        grpButton.Controls[k].Enabled = true;
+                    }
+                }
+            }
+        }
+    }
+}
